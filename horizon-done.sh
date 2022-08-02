be9345baed29d9ccb98aad87023971b7f01bc82a
techtino <36424643+techtino@users.noreply.github.com>
2022-08-02 12:18:02 +0100

    Add horizon scripts
    Disables shortcuts on host so that horizon can work (xwayland grab access rules doesnt work...)

---
+++ horizon-done.sh
@@ -0,0 +1,6 @@
+gsettings set org.gnome.mutter overlay-key 'SUPER'
+gsettings set org.gnome.desktop.wm.keybindings panel-run-dialog "['<Alt>F2']"
+gsettings set org.gnome.desktop.wm.keybindings switch-applications "['<Alt>Tab']"
+gsettings set org.gnome.desktop.wm.keybindings switch-applications-backward "['<Shift><Alt>Tab']"
+gsettings set org.gnome.desktop.wm.keybindings switch-windows "['<Super>Tab']"
+gsettings set org.gnome.desktop.wm.keybindings switch-windows-backward "['<Shift><Super>Tab']"
---
+++ horizon-readyness.sh
@@ -0,0 +1,6 @@
+gsettings set org.gnome.mutter overlay-key ''
+gsettings set org.gnome.desktop.wm.keybindings panel-run-dialog "['']"
+gsettings set org.gnome.desktop.wm.keybindings switch-applications "['']"
+gsettings set org.gnome.desktop.wm.keybindings switch-applications-backward "['']"
+gsettings set org.gnome.desktop.wm.keybindings switch-windows "['']"
+gsettings set org.gnome.desktop.wm.keybindings switch-windows-backward "['']"
